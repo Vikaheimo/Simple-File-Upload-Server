970a8dcc97c4ffe48d75f88c3e6b7a272878551f
Vili Ikäheimo <vili.vikaheim@gmail.com>
2026-02-15 17:48:44 +0900

    feat: server address and upload folder from argument

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use axum::{Router, routing::get};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Environment {
+    /// The address where the server should bind to
+    #[arg(short, long, default_value_t=String::from("localhost:3000"))]
+    pub server_address: String,
+
+    /// Folder where uploads are stored at
+    #[arg(short, long, default_value_t=String::from("./uploads"))]
+    pub folder: String,
+}
+
+lazy_static::lazy_static! {
+    static ref ENVIRONMENT: Environment = Environment::parse();
+}
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/version", get(version_route));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind(&ENVIRONMENT.server_address)
         .await
         .unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
