ac4dfa52cb2889249cfe5663eb937eeb64ae432a
Vili Ikäheimo <vili.vikaheim@gmail.com>
2026-02-15 17:11:09 +0900

    feat: axum setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/version", get(version_route));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn version_route() -> &'static str {
+    env!("CARGO_PKG_VERSION")
 }
