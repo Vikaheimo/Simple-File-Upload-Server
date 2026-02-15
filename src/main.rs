5c895ce1ef018443623b6cf4414da350b4691c87
Vili Ikäheimo <vili.vikaheim@gmail.com>
2026-02-15 18:25:42 +0900

    feat: file uploader struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use axum::{Router, routing::get};
 use clap::Parser;
+use fs2::FileExt;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -13,8 +14,54 @@ struct Environment {
     pub folder: String,
 }
 
+#[derive(Debug)]
+struct FileUploader {
+    #[allow(dead_code)]
+    lock_file: std::fs::File,
+    folder_path: std::path::PathBuf,
+    upload_count: u32,
+}
+
+impl FileUploader {
+    fn new(folder_path: std::path::PathBuf) -> Option<Self> {
+        std::fs::create_dir_all(&folder_path).ok()?;
+
+        let mut lockfile_path = folder_path.clone();
+        lockfile_path.push(".lock");
+        let lock_file = std::fs::File::options()
+            .read(true)
+            .write(true)
+            .truncate(true)
+            .create(true)
+            .open(lockfile_path)
+            .ok()?;
+        lock_file.try_lock_exclusive().ok()?;
+
+        Some(Self {
+            lock_file,
+            folder_path,
+            upload_count: 0,
+        })
+    }
+
+    pub fn init(folder_path: &str) -> Option<Self> {
+        let as_path = std::path::PathBuf::from(folder_path);
+        Self::new(as_path)
+    }
+
+    pub fn print_info(&self) {
+        println!(
+            "Uploaded {} files to '{}'",
+            self.upload_count,
+            self.folder_path.display()
+        );
+    }
+}
+
 lazy_static::lazy_static! {
     static ref ENVIRONMENT: Environment = Environment::parse();
+    static ref FILE_UPLOADER: FileUploader = FileUploader::init(&ENVIRONMENT.folder)
+        .expect("Failed to initialize FileUploader. This application allows only one running instance per upload folder. Check that the upload folder exists or can be created, that the process has permissions to access it, and that no other instance of this application is currently running and holding the lock file.");
 }
 
 #[tokio::main]
@@ -25,6 +72,7 @@ async fn main() {
         .await
         .unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
+    FILE_UPLOADER.print_info();
     axum::serve(listener, app).await.unwrap();
 }
 
